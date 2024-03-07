bda3647e60514ee91820296ce46c09bb38f3aa29
Yuanjun Ren <renyj@buaa.edu.cn>
2024-03-07 16:34:14 +0800

    Add verbose regex expression testcases

--- string_test/string_template.py
+++ string_test/string_template.py
@@ -581,3 +581,378 @@ test_patterns(
 # followed by a sequence that may include either a or b. The patterns are similar, but the resulting matches are
 # completely different.
 
+# Defining group containing a subpattern is also useful in cases where the string matching the subpattern is not part
+# of what should be extracted from the full text. These kinds of groups are called non-capturing.
+# To create a non-capturing group, use the syntax: (?:pattern)
+
+test_patterns(
+    'abbaabbba',
+    [(r'a((a+)|(b+))', 'capturing form'),
+     (r'a((?:a+)|(?:b+))', 'non-capturing')]
+)
+
+# Search Options
+# Option flags are used to change the way the matching engine process an expression. The flags can be combined using a
+# bitwise OR operation, then passed to compile(), search(), match(), and other functions that accept a pattern for
+# searching.
+
+# Case-Insensitive Matching
+# IGNORECASE
+
+text = 'This is some text -- with punctuation.'
+pattern = r'\bT\w+'
+with_case = re.compile(pattern)
+without_case = re.compile(pattern, re.IGNORECASE)
+
+print('Text:\n {!r}'.format(text))
+print('Pattern:\n {}'.format(pattern))
+print('Case-sensitive: ')
+for match in with_case.findall(text):
+    print('  {!r}'.format(match))
+print('Case-insensitive: ')
+for match in without_case.findall(text):
+    print('  {!r}'.format(match))
+
+
+# Input with Multiple Lines
+# Two flags affect how searching in multiline input works: MULTILINE and DOTALL.
+#
+# MULTILINE
+# MULTILINE flag controls how the pattern matching code proceses anchoring instructions for text containing newline
+# characters. When multiline mode is turned on, the anchor rules for ^ and $ apply at the beginning and end of each
+# line, in addition to the entire string.
+
+text = 'This is some text -- with punctuation.\nA second line'
+pattern = r'(^\w+)|(\w+\S*$)'
+single_line = re.compile(pattern)
+multiline = re.compile(pattern, re.MULTILINE)
+
+print('Text:\n {!r}'.format(text))
+print('Pattern:\n {}'.format(pattern))
+print('Single Line: ')
+for match in single_line.findall(text):
+    print('  {!r}'.format(match))
+print('Multiline: ')
+for match in multiline.findall(text):
+    print('  {!r}'.format(match))
+
+
+# DOTALL
+# DOTALL is the other flag related to multiline text. Normally, the dot character(.) matches everything in the input
+# text except a newline character. The flag allows the dot to match newlines as well.
+
+text = 'This is some text -- with punctuation.\nA second line.'
+pattern = r'.+'
+no_newlines = re.compile(pattern)
+dotall = re.compile(pattern, re.DOTALL)
+
+print('Text:\n {!r}'.format(text))
+print('Pattern:\n {}'.format(pattern))
+print('No newlines:')
+for match in no_newlines.findall(text):
+    print('  {!r}'.format(match))
+print('Dotall:')
+for match in dotall.findall(text):
+    print('  {!r}'.format(match))
+
+# A pattern to validate email addresses will illustrate how verbose mode makes working with regular expressions eaiser.
+
+address = re.compile('[\w\d.+-]+@([\w\d.]+\.)+(com|org|edu)')
+candidates = [
+    r'first.last@example.com',
+    r'first.last+category@gmail.com',
+    r'valid-address@mail.example.com',
+    r'not-valid@example.foo',
+]
+
+for candidate in candidates:
+    match = address.search(candidate)
+    print('{:<30} {}'.format(
+        candidate, 'Matches' if match else 'No match'))
+
+
+# Converting the expression to a more verbose format will make it easier to extend.
+
+address = re.compile(
+    '''
+    # A name is made up of letters, and may include "."
+    # for title abbreviations and middle initials.
+    ((?P<name>
+        ([\w.,]+\s+)*[\w.,]+)
+        \s*
+        # Email addresses are wapped in angle
+        # brackets < >, but only ifa name is
+        # found, so keep the start bracket in
+        # this group.
+        <
+    )? # The entire name is optional.
+    
+    # The address itself: username@domain.tld
+    (?P<email>
+        [\w\d.+-]+      # Username
+        @
+        ([\w\d.]+\.)+   # Domain name prefix
+        (com|org|edu)   # Limit the allowed top-level domains.
+    )
+    >? # Optional closing angle bracket.
+    ''',
+    re.VERBOSE)
+
+candidates = [
+    u'first.last@example.com',
+    u'first.last+category@gmail.com',
+    u'valid-address@mail.example.com',
+    u'not-valid-address@example.foo',
+    u'First Last <first.last@example.com>',
+    u'No Brackets first.last@example.com',
+    u'First Last',
+    u'First Middle Last <first.last@example.com>',
+    u'First M. Lst <first.last@example.com>',
+    u'<first.last@example.com>'
+]
+
+for candidate in candidates:
+    match = address.search(candidate)
+    if match:
+        print('  Name: ', match.groupdict()['name'])
+        print('  Email: ', match.groupdict()['email'])
+    else:
+        print('  No Match')
+
+# Embedding Flags in Patterns
+# Turn case-insensitive matching on, add (?i) to the beginning of the expression.
+
+text = 'This is some text -- with punctuation.'
+pattern = r'(?i)\bT\w+'
+regex = re.compile(pattern)
+
+print('Text: ', text)
+print('Pattern: ', pattern)
+print('Matches: ', regex.findall(text))
+
+
+# Looking Ahead or Behind
+# Positive look ahead assertion syntax: (?=pattern)
+
+
+print("Looking Ahead or Behind")
+
+address = re.compile(
+    '''
+    # A name is made up of letters, and may include "."
+    # for title abbreviations and middle initials.
+    ((?P<name>
+        ([\w.,]+\s+)*[\w.,]+
+      )
+      \s+
+    ) # The name is no longer optional
+    
+    # LOOKAHEAD
+    # Email addresses are wrapped in angle brackets, but only if both
+    # are present or neither is.
+    (?= (<.*>$)         # Remainder wrapped in angle brackets
+        |
+        ([^<].*[^>]$)   # Remainder *not* wrapped in angle brackets
+    )
+    <? # Optional opening angle bracket
+    
+    # The address itself: username@domain.tld
+    (?P<email>
+        [\w\d.+-]+      # Username
+        @
+        ([\w\d.]+\.)+   # Domain name prefix
+        (com|org|edu)   # Limit the allowed top-level domains.
+    )
+    >?  # Optional closing angle bracket
+    ''',
+    re.VERBOSE)
+
+candidates = [
+    u'First Last <first.last@example.com>',
+    u'No Brackets first.last@example.com',
+    u'Open Bracket <first.last@example.com',
+    u'Close Bracket <first.last@example.example>',
+]
+
+for candidate in candidates:
+    print('Candidate: ', candidate)
+    match = address.search(candidate)
+    if match:
+        print('  Name: ', match.groupdict()['name'])
+        print('  Email: ', match.groupdict()['email'])
+    else:
+        print('  No Match')
+
+
+# The negative look ahead assertion (?!pattern) says that the pattern does not match the text following the current
+# point.
+print("Negative Look Ahead Assertion")
+
+address = re.compile(
+    '''
+    ^
+    # An address: username@domain.tld
+    
+    # Ignore noreply addresses.
+    (?!noreply@.*$)
+    [\w\d.+-]+      # Username
+    @
+    ([\w\d.]+\.)+   # Domain name prefix
+    (com|org|edu)   # Limit the allowed top-level domains.
+    
+    $
+    ''',
+    re.VERBOSE)
+
+candidates = [
+    u'first.last@example.com',
+    u'noreply@example.com',
+]
+
+for candidate in candidates:
+    print('Candidate: ', candidate)
+    match = address.search(candidate)
+
+    if match:
+        print('  Match: ', candidate[match.start():match.end()])
+    else:
+        print('  No match')
+
+
+# The negative look behind assertion after the username is matched using the syntax (?<!pattern).
+
+print('Negative Look behind Assertion:')
+
+address = re.compile(
+    '''
+    ^
+    # An address: username@domain.tld
+    [\w\d.+-]+  # Username
+    
+    # Ignore noreply addresses.
+    (?<!noreply)
+    
+    @
+    ([\w\d.]+\.)+   # Domain name prefix
+    (com|org|edu)   # Limit the allowed top-level domains
+    
+    $
+    ''',
+    re.VERBOSE)
+
+candidates = [
+    u'first.last@example.com',
+    u'noreply@example.com',
+]
+
+for candidate in candidates:
+    print('Candidate: ', candidate)
+    match = address.search(candidate)
+    if match:
+        print('  Match: ', candidate[match.start():match.end()])
+    else:
+        print('  No match')
+
+
+# Positive Look Ahead Assertion
+# The positive look ahead assertion can be used to find text following a pattern using the syntax (?<=pattern).
+
+twitter = re.compile(
+    '''
+    # A twitter handle: @username
+    (?<=@)
+    ([\w\d_]+)  # Username
+    ''',
+    re.VERBOSE)
+
+text = '''This text includes two Twitter handles.
+One for @ThePSF, and one for the author, @doughellmann.
+'''
+
+print(text)
+for match in twitter.findall(text):
+    print('Handle: ', match)
+
+
+# Self-Referencing Expressions
+# Matched values can be used in later parts of an expression. The easiest way to achieve this is by referring to the
+# previously matched group by ID number, using \num.
+
+address = re.compile(
+    r'''
+    # The regular name
+    (\w+)   # First name
+    \s+
+    (([\w.]+)\s+)?  # Optional middle name or initial
+    (\w+)           # Last name
+    
+    \s+
+    
+    <
+    
+    # The address: first_name.last_name@domain.tld
+    (?P<email>
+        \1      # First name
+        \.
+        \4      # Last name
+        @
+        ([\w\d.]+\.)+   # Domain name prefix
+        (com|org|edu)   # Limit the allowed top-level domains.
+    )
+    >
+    ''',
+    re.VERBOSE | re.IGNORECASE)
+
+candidates = [
+    u'First Last <first.last@example.com>',
+    u'Different Name <first.last@example.com>',
+    u'First Middle Last <first.last@example.com>',
+    u'First M. Last <first.last@example.com>',
+]
+
+for candidate in candidates:
+    print('Candidate: ', candidate)
+    match = address.search(candidate)
+    if match:
+        print('  Match name: ', match.group(1), match.group(4))
+        print('  Match email: ', match.group(5))
+    else:
+        print('  No Match')
+
+# Although the syntax is simple, creating back-references by numerical ID has a few disadvantages. From a practical
+# standpoint, as the expression changes, the groups must be counted again and every reference may need to be updated.
+# Another disadvantage is that ony 99 references can be made using the standard back-reference syntax \n, because
+# if the ID number is three digits long, it will be interpreted as an octal character value instead of a group
+# reference. Of course, if there are more than 99 groups in an expression, there will be more serious maintenance
+# challenges than simply not being able to refer to all of them.
+#
+# Python's expression parser includes an extension that uses (?P=name) to refer to the value of a named group matched
+# earlier in the expression.
+
+address = re.compile(
+    '''
+    # The regular name
+    (?P<first_name>\w+)
+    \s+
+    (([\w.]+)\s)?   # Optinal middle name or initial
+    (?P<last_name>\w_)
+    
+    \s+
+    
+    <
+    
+    # The address: first_name.last_name@domain.tld
+    (?P<email>
+        (?P=first_name)
+        \.
+        (?P=last_name)
+        @
+        ([\w\d.]+\.)+   # Domain name prefix
+        (com|org|edu)   # Limit the allowed top-level domains.
+    )
+    ''',
+    re.VERBOSE | re.IGNORECASE)
+
+
+
+
